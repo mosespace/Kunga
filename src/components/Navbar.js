d78ddffc336dbb9df52b728ef65d2ca3597d9217
mosespace <kisakyemoses50@gmail.com>
2023-03-31 19:48:24 +0300

    feat: updated content ✨

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,13 @@ import { Link } from 'gatsby'
 export default function Navbar(){
     return(
         <nav>
-            <h1>Hello World</h1>
+            <h1>Logo</h1>
             <div className="links">
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
                 <Link to='/events'>Events</Link>
                 <Link to='/contact'>Contact us</Link>
+                <Link to='/projects'>Portifolio Projects</Link>
             </div>
         </nav>
     )
