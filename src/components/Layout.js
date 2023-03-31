4f9b9a086aefd18fddcb1b7d9f53fb253e62943d
mosespace <kisakyemoses50@gmail.com>
2023-03-31 19:47:34 +0300

    feat: updated content ✨

---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import '../styles/global.css'
+
+export default function Layout({children}){
+    return(
+        <div className='layout'>
+            <Navbar/>
+            <div className='content'>
+                {children}
+            </div>
+            <footer>
+            <p>This is the footer section of our Kunga Website</p>
+            </footer>
+        </div>
+     
+    )
+}
