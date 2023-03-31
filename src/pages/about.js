d51e89f7c6ff1b81af3f6a7e695be07efc7f3bc4
mosespace <kisakyemoses50@gmail.com>
2023-03-31 17:35:06 +0300

    feat: updated the code lines

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,19 @@
-// Step 1: Importing React from the 'react' package, so that we can use JSX inside our .js file.
-import * as React from 'react'
+import React from 'react'
+import Navbar from '../components/Navbar'
+
 
 /* Step 2: Defining our component. Note that our
 component name should start with a capital letter. */
-const Aboutpage = () => {
+const AboutPage = () => {
+    
     return (
-      <h1>Hi, welcome to my site!</h1>
+        <>
+        <Navbar/>
+      <h1>Hi, welcome to my about site!</h1>
+      </>
     )
   }
 
   /* Step 3: Exporting our component so it
 can be used by other parts of our app. */
-  export default Aboutpage
+  export default AboutPage
