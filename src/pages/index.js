e551b7bae4fe172a7fb7a06899f8ef1095b199a8
mosespace <kisakyemoses50@gmail.com>
2023-03-31 19:55:06 +0300

    feat: updated content ✨

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,17 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import Layout from '../components/Layout';
 
+export default function Home(){
+  return (
+    <Layout>
+      <section>
+        <div>
+          <h2>Kunga</h2>
+          <h3>Discover and joinany events closer to you!</h3>
 
-/* Step 2: Defining our component. Note that our
-component name should start with a capital letter. */
-const HomePage = () => {
-    
-    return (
-        <>
-        <Navbar/>
-      <h1>Hi, welcome to my site!</h1>
-      <Footer/>
-      </>
-    )
-  }
-
-  /* Step 3: Exporting our component so it
-can be used by other parts of our app. */
-  export default HomePage
+          <p>Whatever your interest, from coding and reading to networking and skill sharing, there are thousands of people who share it on <code>Kunga</code>. Events are happening every day—log in to join the fun</p>
+        </div>
+      </section>
+    </Layout>
+  )
+}
