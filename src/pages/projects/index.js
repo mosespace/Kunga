9bc9060dadf4937f5cd0f03b5edf58a683b1b295
mosespace <kisakyemoses50@gmail.com>
2023-03-31 19:49:27 +0300

    feat: updated content ✨

---
+++ src/pages/projects/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default function Home(){
+  return (
+    <Layout>
+      <section>
+        <div>
+          <h2>Kunga</h2>
+          <h3>Kunga Portfolio</h3>
+          <p>Discover and join any events closer to you!</p>
+        </div>
+      </section>
+    </Layout>
+  )
+}
