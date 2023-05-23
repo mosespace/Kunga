ff5a37142ce521ffbefc0d3384e0958028ee3b2a
mosespace <kisakyemoses50@gmail.com>
2023-05-23 11:03:16 +0300

    feat: initial commit

---
+++ src/pages/backend/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default function Home(){
+  return (
+    <Layout>
+      <section>
+        <div>
+          <h2>This will be the Backend</h2>
+          <h3>Of the Official Kunga Portfolio Website</h3>
+          <p>Discover and join any events closer to you!</p>
+        </div>
+      </section>
+    </Layout>
+  )
+}
